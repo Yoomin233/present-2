66c76da6e7458c4e9555dd2ab18aca0a993b986e
Yuemin.Hu <hym920408@gmail.com>
2018-04-11 18:04:48 +0800

    bricking...

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const http = require('http')
 const parseCookie = require('./parseCookie')
+
 const fse = require('fs-extra')
+const path = require('path')
+
+const qs = require('querystring')
+const keyword = '123456'
+
 const storage = {}
 http
   .createServer(async (req, res) => {
@@ -29,25 +35,73 @@ http
     // } else {
     //   req.cookie.visitCount = 1
     // }
-    if (req.url === '/' ) {
+    if (req.url === '/' && req.method === 'GET') {
       res.writeHead(200, {
         'Content-Type': 'text/html'
       })
       if (req.cookie.auth) {
-        res.end(
-          `Hello world!\nyou have visit this page ${
-            req.cookie.visitCount
-          } time(s)`
-        )
+        res.end(`Hello! You should see this page after auth pass!`)
       } else {
+        // 跳转到认证页面
         const content = await fse.readFile('./static/auth.html')
         res.end(content)
       }
+    } else if (req.url.includes('/validate') && req.method === 'POST') {
+      // 这里读出post data
+      let body = ''
+      req.on('data', data => {
+        body += data
+      })
+      req.on('end', () => {
+        const POST = qs.parse(body)
+        if (POST.valueContainer === keyword) {
+          req.cookie.auth = true
+          res.writeHead(302, {
+            Location: encodeURI('/')
+          })
+          res.end('')
+          // res.writeHead(200, { 'Content-Type': 'text/plain' })
+          // res.end('validate success!')
+        } else {
+          res.writeHead(302, {
+            Location: encodeURI('/')
+          })
+          res.end('validate error!')
+        }
+      })
+    } else if (req.url.includes('/static') && req.method === 'GET') {
+      const fileName = path.join(__dirname, '.', req.url)
+      const extensionFileName = req.url.match(/\.([a-zA-Z0-9]*)$/)[1]
+      switch (extensionFileName) {
+        case 'css':
+          res.writeHead(200, {
+            'Content-Type': 'text/css; charset="utf-8"'
+          })
+          break
+        case 'png':
+          res.writeHead(200, `Content-Type: image/${extensionFileName}`)
+          break
+        case 'jpeg':
+          res.writeHead(200, `Content-Type: image/${extensionFileName}`)
+          break
+        case 'mp3':
+          res.writeHead(200, `Content-Type: video/${extensionFileName}`)
+          break
+        case 'wav':
+          res.writeHead(200, `Content-Type: video/${extensionFileName}`)
+          break
+        case 'ogg':
+          res.writeHead(200, `Content-Type: image/${extensionFileName}`)
+          break
+        default:
+          res.writeHead(200, { 'Content-Type': 'text/plain' })
+          break
+      }
+      const content = await fse.readFile(fileName)
+      res.end(content)
     } else {
       res.writeHead(404, { 'Content-Type': 'text/plain' })
-      res.end(
-        `page not found`
-      )
+      res.end(`page not found`)
     }
   })
   .listen(8082)
